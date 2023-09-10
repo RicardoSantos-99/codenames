f276155d324c159902395baa42f9f2283551413e
RicardoSantos-99 <ricardo2010carvalho@hotmail.com>
2023-09-10 18:52:10 -0300

    feat: update the list of users already in the room closes #1

--- lib/codenames_web/live/game_live/index.ex
+++ lib/codenames_web/live/game_live/index.ex
@@ -2,7 +2,7 @@ defmodule CodenamesWeb.GameLive.Index do
   use CodenamesWeb, :live_view
 
   import CodenamesWeb.Components.{Card, Team}
-  alias CodenamesWeb.{Endpoint, Presence}
+  alias CodenamesWeb.{Presence}
   alias Codenames.Board
   alias Phoenix.Socket.Broadcast
   alias Phoenix.PubSub
@@ -13,35 +13,19 @@ defmodule CodenamesWeb.GameLive.Index do
 
     PubSub.subscribe(Codenames.PubSub, "game_room")
     Presence.track(self(), "game_room", user.email, user)
-    Endpoint.subscribe("game_room")
 
-    socket = assign(socket, :user_emails, fetch_current_emails())
+    socket = assign(socket, :user_emails, [])
     socket = assign(socket, :board, Board.build_game_board())
 
     {:ok, socket}
   end
 
   @impl true
-  def handle_info(%Broadcast{event: "presence_diff", payload: payload, topic: _topic}, socket) do
-    _joins = Map.get(payload, :joins)
-    _leaves = Map.get(payload, :leaves)
-
-    {:noreply, reload_users(socket)}
-  end
-
-  def reload_users(socket) do
+  def handle_info(%Broadcast{event: "presence_diff", payload: _payload, topic: _topic}, socket) do
     users =
       Presence.list("game_room")
-      |> Enum.map(fn {_user_id, data} ->
-        List.first(data[:metas])
-      end)
+      |> Enum.map(fn {_user_id, data} -> List.first(data[:metas]).email end)
 
-    socket
-    |> assign(:users, users)
-  end
-
-  defp fetch_current_emails() do
-    Presence.list("game_room")
-    |> Enum.map(fn {email, _meta} -> email end)
+    {:noreply, socket |> assign(:user_emails, users)}
   end
 end
